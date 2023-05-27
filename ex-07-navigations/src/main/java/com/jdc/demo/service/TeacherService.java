05ff1c044065cada97c1c1b66308f03dbb01905b
Min Lwin <lwin.zawmin@gmail.com>
2023-05-27 15:15:18 +0630

    Add Teacher Service

---
+++ ex-07-navigations/src/main/java/com/jdc/demo/service/TeacherService.java
@@ -0,0 +1,28 @@
+package com.jdc.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.jdc.demo.service.entity.Teacher;
+
+@Service
+public class TeacherService {
+
+	public List<Teacher> search(Optional<String> name, Optional<String> phone) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Teacher findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Integer save(Teacher form) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
