74779b3d920afef9150cb1adf52520ff9a4511c0
Min Lwin <lwin.zawmin@gmail.com>
2023-06-24 10:12:56 +0630

    Share Spring Boots Project

---
+++ ex-14-thymeleaf-binding/src/test/java/com/jdc/demo/binding/Ex14ThymeleafBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdc.demo.binding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex14ThymeleafBindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
