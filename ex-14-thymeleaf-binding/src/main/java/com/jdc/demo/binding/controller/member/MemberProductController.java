e49c2a4a36342b65f1031e151d8f3b7d2703e745
Min Lwin <lwin.zawmin@gmail.com>
2023-07-09 08:55:07 +0630

    Update Product Edit View

--- ex-14-thymeleaf-binding/src/main/java/com/jdc/demo/binding/controller/member/MemberProductController.java
+++ ex-14-thymeleaf-binding/src/main/java/com/jdc/demo/binding/controller/member/MemberProductController.java
@@ -5,6 +5,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,15 @@ import com.jdc.demo.binding.domain.dto.form.ProductForm;
 @Controller
 @RequestMapping("member/product")
 public class MemberProductController {
+	
+	@GetMapping("{id}")
+	public String findById(@PathVariable int id) {
+		return "member/product/details";
+	}
 
 	@GetMapping("edit")
 	public String edit(@RequestParam int shop) {
-		return "member/product-edit";
+		return "member/product/edit";
 	}
 	
 	@PostMapping("edit")
@@ -25,14 +31,16 @@ public class MemberProductController {
 			BindingResult bindingResult) {
 		
 		if(bindingResult.hasErrors()) {
-			return "member/product-edit";
+			return "member/product/edit";
 		}
 		
-		return "";
+		return "redirect:/member/product/%d".formatted(1);
 	}
 	
 	@ModelAttribute("form")
-	public ProductForm form() {
-		return new ProductForm();
+	public ProductForm form(@RequestParam int shop) {
+		var form = new ProductForm();
+		form.setShop(shop);
+		return form;
 	}
 }
