f8a8477ccdbe0055c509dda7d48f6fc42e8742ee
Min Lwin <lwin.zawmin@gmail.com>
2023-07-15 12:44:21 +0630

    Add Mouse Over Shadow Effect

--- ex-14-thymeleaf-binding/src/main/resources/static/js/product-photo-select.js
+++ ex-14-thymeleaf-binding/src/main/resources/static/js/product-photo-select.js
@@ -4,8 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 	const cover = document.getElementById('coverImage')
 	
 	if(controls && cover) {
+
+		cover.addEventListener('mouseenter', () => cover.classList.add('shadow'))
+		cover.addEventListener('mouseleave', () => cover.classList.remove('shadow'))
+		
 		Array.from(controls).forEach(image => {
 			image.addEventListener('click', () => cover.src = image.src)
+			image.addEventListener('mouseenter', () => image.classList.add('shadow'))
+			image.addEventListener('mouseleave', () => image.classList.remove('shadow'))
 		})
+		
 	}
 })
