43f1637a6892e073030afb6827b070102b6c193b
pphyo <pyaephyo.jdc@gmail.com>
2023-07-30 07:59:12 +0630

    Angular Reactive forms

--- angular/angular-works/projects/the-forms/src/app/app.module.ts
+++ angular/angular-works/projects/the-forms/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { TemplateComponent } from './template/template.component';
 import { ReactiveComponent } from './reactive/reactive.component';
 import { DynamicComponent } from './dynamic/dynamic.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/angular-works/projects/the-forms/src/app/reactive/reactive.component.ts
+++ angular/angular-works/projects/the-forms/src/app/reactive/reactive.component.ts
@@ -1,9 +1,48 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html'
 })
 export class ReactiveComponent {
+
+  countries = [
+    'Myanmar', 'Thailand', 'Korea',
+    'Japan', 'Vietnam',
+    'Phillipine', 'Indonesia', 'Malaysia', 'Cambodia'
+  ]
+
+  form:FormGroup
+
+  constructor(builder: FormBuilder) {
+    this.form = builder.group({
+      name: ['', Validators.required],
+      contact: builder.group({
+        email: ['', Validators.required],
+        phone: '',
+      }),
+      country: '',
+      hobbies: new FormArray([
+        new FormControl
+      ])
+    })
+  }
+
+  get hobbies() {
+    return this.form.get('hobbies') as FormArray
+  }
+
+  addHobby() {
+    this.hobbies.push(new FormControl)
+  }
+
+  remove(index: number) {
+    this.hobbies.removeAt(index)
+  }
+
+  save() {
+    console.log(this.form)
+  }
 
 }
