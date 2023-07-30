21c2c5afe0401ee87e4fee2e4415700eb756e5a9
jdc <jdc@LAPTOP-V07667SA>
2023-07-30 15:12:51 +0630

    Update Create Township

--- spring-rest/02-location-api/src/main/java/com/jdc/location/model/service/TownshipService.java
+++ spring-rest/02-location-api/src/main/java/com/jdc/location/model/service/TownshipService.java
@@ -4,23 +4,36 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jdc.location.model.dto.TownshipDTO;
 import com.jdc.location.model.dto.form.TownshipForm;
+import com.jdc.location.model.repo.DivisionRepo;
+import com.jdc.location.model.repo.TownshipRepo;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TownshipService {
 
+	private final TownshipRepo townshipRepo;
+	private final DivisionRepo divisionRepo;
+	
 	public TownshipDTO findById(int id) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Transactional
 	public TownshipDTO create(TownshipForm form) {
-		// TODO Auto-generated method stub
-		return null;
+		var entity = form.entity(divisionRepo::findById);
+		entity = townshipRepo.save(entity);
+		return TownshipDTO.from(entity);
 	}
 
+	@Transactional
 	public TownshipDTO update(int id, TownshipForm form) {
 		// TODO Auto-generated method stub
 		return null;
