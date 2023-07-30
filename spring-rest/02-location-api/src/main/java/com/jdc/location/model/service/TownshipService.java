27f742fce67a826170987ca11182df9fd6ea7ef3
minlwin <lwin.zawmin@gmail.com>
2023-07-30 15:46:45 +0630

    Update Assignment

--- spring-rest/02-location-api/src/main/java/com/jdc/location/model/service/TownshipService.java
+++ spring-rest/02-location-api/src/main/java/com/jdc/location/model/service/TownshipService.java
@@ -3,11 +3,14 @@ package com.jdc.location.model.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.jdc.location.model.LocationBusinessException;
 import com.jdc.location.model.dto.TownshipDTO;
 import com.jdc.location.model.dto.form.TownshipForm;
+import com.jdc.location.model.entity.Township;
 import com.jdc.location.model.repo.DivisionRepo;
 import com.jdc.location.model.repo.TownshipRepo;
 
@@ -22,8 +25,8 @@ public class TownshipService {
 	private final DivisionRepo divisionRepo;
 	
 	public TownshipDTO findById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		return townshipRepo.findById(id).map(TownshipDTO::from)
+				.orElseThrow(() -> new LocationBusinessException("There is no Township with id %d.".formatted(id)));
 	}
 
 	@Transactional
@@ -35,13 +38,46 @@ public class TownshipService {
 
 	@Transactional
 	public TownshipDTO update(int id, TownshipForm form) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		var entity = form.entity(divisionRepo::findById);
+		
+		return townshipRepo.findById(id).map(e -> {
+			e.setName(form.name());
+			e.setBurmese(form.burmese());
+			e.setDivision(entity.getDivision());
+			return TownshipDTO.from(e);
+		})
+		.orElseThrow(() -> new LocationBusinessException("There is no Township with id %d.".formatted(id)));
 	}
-
+	
 	public List<TownshipDTO> search(Optional<String> region, Optional<Integer> division, Optional<String> keyword) {
-		// TODO Auto-generated method stub
-		return null;
+		return townshipRepo.findAll(division(region, division).and(keyword(keyword)))
+				.stream().map(TownshipDTO::from).toList();
 	}
-
+	
+	private Specification<Township> division(Optional<String> region, Optional<Integer> division) {
+		
+		if(division.isPresent()) {
+			return (root, query, cb) -> cb.equal(root.get("division").get("id"), division.get());
+		}
+		
+		if(region.isPresent()) {
+			return (root, query, cb) -> cb.equal(root.get("division").get("region"), region.get());
+		}
+		
+		return Specification.where(null);
+	}
+	
+	private Specification<Township> keyword(Optional<String> param) {
+		
+		if(param.isPresent()) {
+			return (root, query, cb) -> cb.or(
+				cb.like(cb.lower(root.get("name")), param.get().toLowerCase().concat("%")),
+				cb.like(root.get("burmese"), param.get().concat("%"))
+			);
+		}
+		
+		return Specification.where(null);
+	}
+	
 }
